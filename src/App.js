893e376fc0886407d073693995f18a0427453d5e
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-02-10 05:18:15 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import Counter from './components/Counter';
+import './styles/header.css'
+import './styles/footer.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <div>
+    <Header />
+    <Footer />
+
+  {/* <Counter /> */}
+
+ </div>
+    );
 }
 
 export default App;
